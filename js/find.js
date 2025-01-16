66bf77e9f45a78a77c40774c1364ae6425eac5d0
handvi <faisalhanafi98.jobs@gmail.com>
2025-01-17 03:39:50 +0700

    fixed

--- js/find.js
+++ js/find.js
@@ -1,1 +1,17 @@
-console.log('serach buton');
+document.getElementById('search-input').addEventListener('input', function(e) {
+    const query = e.target.value.toLowerCase(); 
+    const articles = document.querySelectorAll('.article'); 
+
+    
+    articles.forEach(article => {
+        const title = article.querySelector('.card-body h5').textContent.toLowerCase(); /
+        const description = article.querySelector('.card-body p').textContent.toLowerCase(); 
+
+        
+        if (title.includes(query) || description.includes(query)) {
+            article.style.display = 'block'; 
+        } else {
+            article.style.display = 'none'; 
+        }
+    });
+});
